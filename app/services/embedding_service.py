9b33a110be5dcf56c665fee337b8ed215e2edd8c
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-11-21 22:44:12 +0530

    rename get_embedding_model to embedding_model for consistency

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,4 +1,4 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-def get_embedding_model(model_name="models/gemini-embedding-001"):
+def embedding_model(model_name="models/gemini-embedding-001"):
     return GoogleGenerativeAIEmbeddings(model=model_name)
