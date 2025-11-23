5e08d14c48940d776de74fb3f5a1465f29962c9d
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-11-23 23:39:37 +0530

    feat: integrate multiple routers for chat initialization, RAG, and summarization

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from routers.chat_init_route import router as chat_init_router
+from routers.rag_route import router as rag_router
+from routers.summarize_route import router as summarize_route
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+router = APIRouter()
 
-@app.get("/")
+
+
+
+@router.get("/")
 async def read_root():
     return {"Hello": "World"}
-  
+
+
+app.include_router(chat_init_router, prefix= "/chat_init")
+app.include_router(rag_router, prefix="/ask")
+app.include_router(summarize_route, prefix="/summarize")
+app.include_router(router)
