1669429665b3972eee3040faf005a8ef68f521ea
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-11-23 23:29:52 +0530

    refactor: replace initialize_pinecone with get_pinecone for improved client management

--- app/services/vector_db_service.py
+++ app/services/vector_db_service.py
@@ -1,11 +1,17 @@
 import os
 from pinecone import Pinecone, ServerlessSpec
 
-def initialize_pinecone():
-    api_key = os.getenv("PINECONE_API_KEY")
-    if not api_key:
-        raise ValueError("PINECONE_API_KEY missing")
-    return Pinecone(api_key=api_key)
+pinecone_client = None
+def get_pinecone():
+    global pinecone_client
+
+    if pinecone_client is None:
+        api_key = os.getenv("PINECONE_API_KEY")
+        if not api_key:
+            raise ValueError("PINECONE_API_KEY missing")
+        pinecone_client = Pinecone(api_key=api_key)
+
+    return pinecone_client
 
 
 def create_index_if_not_exists(pc, name: str, dimension=3072):
